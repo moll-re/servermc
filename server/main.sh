41dd3a8796cb8c3bd21841c363fc0909da14e74b
Remy Moll <mollator2@gmail.com>
2020-06-27 17:38:42 +0200

    now with periodic things

--- server/main.sh
+++ server/main.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #launched through crontab every 10 mins
 
+cd /home/mc/tekxit3
+
 if pgrep -x "java" > /dev/null
 then
-    echo "Still running"
+    mcwrapper cmd say "§l Please remember to run /stop before leaving the server"
 else
     shutdown now
 fi
--- server/reminder.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#launched through crontab every 5-10 mins
-
-cd ~/tekxit3
-mcwrapper cmd say "§l Please remember to run /stop before leaving the server"
