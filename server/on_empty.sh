028c445002f0ab8f50e2c358d96b79f92ecbd088
Remy Moll <mollator2@gmail.com>
2020-07-18 23:25:06 +0000

    fine now

--- server/on_empty.sh
+++ server/on_empty.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 #cron every five minutes
 
-OUT = $(mark2 send list)
-if [[$OUT == ""]] then
+OUT=$(mark2 send list)
+if [[ $OUT == "" ]]
+then
   for i in {1..20}
   do
-    OUT = $(mark2 send list)
-    if [[$OUT != ""]] then
-      exit 1
-    else
+    OUT=$(mark2 send list)
+    if [[ $OUT == "" ]]
+    then
       sleep 30
+    else
+      exit 1
+    fi
   done
 fi
 mark2 stop
