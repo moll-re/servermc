8c034fe1cb98cc08e34f8275dab50463e21daa06
Remy Moll <mollator2@gmail.com>
2021-03-28 19:25:24 +0200

    purged the plugin config

--- server/copy_config.sh
+++ server/copy_config.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cp -R ./plugins/. /srv/craftbukkit/plugins
+cp -rf ./plugins/. /srv/craftbukkit/plugins
+# every file in the local folder will overwrite the config in /srv/crafbukkit/plugins.
+# Remember there might be more files already
 chown -hR craftbukkit /srv/craftbukkit/
+
 
 cp conf.d /etc/conf.d/spigot
 
